e2f4f4d89d6be3625ad010fa89b063ba368aae87
Alekhkr <alekhsub@gmail.com>
2026-02-20 23:26:02 +0530

    ..

--- cf/day1B.cpp
+++ cf/day1B.cpp
@@ -4,9 +4,29 @@
 using namespace std;
 
 void solve(){
-	int n, k ,t;
+	int n, k ,t,idx=0;
 	cin >> n >> k >> t;
+	double val = n*k*t*0.01;
+	double alpha = k *0.01*100;
+	//cout << val << " " <<  alpha << endl;
+	while (idx < n && val >= alpha) {
+		cout << k << " ";
+		alpha += k;
+		idx += 1;
+	}
+	if (idx == n) return;
+	alpha -= k;
+	double ai = val-alpha;
+	//ai += 1;
 	
+	cout << (ai>=1?int(ai):0) << " ";
+	idx += 1;
+	while (idx < n) {
+		cout << 0 << " ";
+		idx++;
+	}
+	cout << endl;
+}
 	
 	
 int main(){
